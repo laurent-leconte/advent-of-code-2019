84215ddf5efca2166fa3bde408726f6c409f342d
Laurent Leconte <laurent.leconte@moonshot-internet.com>
2020-01-13 09:59:02 +0100

    part 2 of day 10

--- day10.ml
+++ day10.ml
@@ -17,7 +17,7 @@ module Direction =
     * 0 = 0
     * 0 < _
     * a = b iff same direction and orientation 
-    * otherwise angle from y-axis is used for comparison *)
+    * otherwise clockwise angle from y-axis is used for comparison *)
       if p = q          then 0    (* handles equality, including 0 = 0 *)
       else if is_zero p then (-1) (* 0 is smaller than everything *)
       else if is_zero q then 1
@@ -32,37 +32,38 @@ module Direction =
 
 module LinesOfSight = Set.Make(Direction)
 
+(* helper functions *)
 let string_of_point p = Printf.sprintf "(%d, %d)" p.x p.y
+
+let string_of_point_list l =
+  "[" ^ (l |> List.map string_of_point |> String.concat ";") ^ "]"
 
 let print_directions =
   let print_point p = print_endline (string_of_point p) in
   LinesOfSight.iter print_point
 
-let rec gcd a = function
-  | 0 -> a
-  | b -> gcd b (a mod b)
-
-let delta p1 p2 = {x = p2.x - p1.x; y = p2.y - p1.y}
+(* directions are stored with traditional coordinates (y points up)
+   whereas Y coordinate for meteors point down *)
+let delta p1 p2 = {x = p2.x - p1.x; y = p1.y - p2.y}
 
 (* count how many other points are visible from `here` *)
 let count_visible points here =
   let direction_from_here = delta here in
-  (points 
+  let count = (points 
   |> List.map direction_from_here (* build all directions between `here` and the list of points *)
   |> List.fold_left (fun s elt -> LinesOfSight.add elt s) LinesOfSight.empty (* put them into a set to keep uniques *)
-  |> LinesOfSight.cardinal) - 1 (* return the size of the set minus 1 to account for `here` *)
+  |> LinesOfSight.cardinal) - 1 in (* count is size of the set minus 1 to account for `here` *)
+  (count, here)
 
 (* copy a list *)
 let rec copy = function
   | []    -> []
   | a::tl -> a::(copy tl)
   
-(* find the max of an int list *)
+(* find the max of an (int, point) list *)
 let max_of_list l =
-  let rec loop max_so_far = function
-    | [] -> max_so_far
-    | a::tl -> loop (max a max_so_far) tl in
-  loop min_int l
+  let max a b = if fst a > fst b then a else b in
+  List.fold_left max (List.hd l) (List.tl l)
 
 let count_all_visibles coords =
   let copy_coords = copy coords in
@@ -94,10 +95,68 @@ let rec test_compare = function
  [] | [_] -> ()
  |a::(b::_ as tl) -> Printf.printf "%s - %s : %d\n" (string_of_point a) (string_of_point b) (Direction.compare a b);
                      test_compare tl
-  
-let _ = "day10.dat" 
+
+(* part 1 *)
+let max, spot = "day10.dat" 
   |> read_lines
   |> points_from_input
   |> count_all_visibles
   |> max_of_list
-  |> print_int
+
+let () = Printf.printf "Part 1: %d (%d, %d)\n" max spot.x spot.y
+  
+  
+(* insert a point in a list, keeping it sorted by increasing distance *)
+let rec insert_by_distance p = function
+  | []    -> [p]
+  | a::tl -> let px, py = p.x, p.y in
+             let ax, ay = a.x, a.y in
+             if px*px + py*py < ax*ax + ay*ay then
+               p::a::tl
+             else
+               a::(insert_by_distance p tl)
+
+module AlignedPoints = Map.Make(Direction)
+
+let add_point m p =
+  match AlignedPoints.find_opt p m with
+    | None   -> AlignedPoints.add p [p] m
+    | Some l -> AlignedPoints.add p (insert_by_distance p l) m
+
+let build_alignment_from here points =
+  let direction_from_here = delta here in
+  points 
+  |> List.map direction_from_here (* build all directions between `here` and the list of points *)
+  |> List.fold_left add_point AlignedPoints.empty (* add them to a direction -> points map *)
+
+(* equivalent to List.map List.tl but ignores empty lists and empty tails *)
+let rec map_tl = function
+  | []      -> []
+  | []::xs 
+  | (_::[])::xs  -> map_tl xs
+  | (_::xs)::xss -> xs :: map_tl xss
+
+(* transpose and flatten a list of list. The lists may be of different lenghths *)
+let rec flat_transpose = function
+  | [] -> []
+  | []::xs -> flat_transpose xs  
+  | x      -> let first_items = List.map List.hd x in
+              let rest =  map_tl x in
+              (*print_endline ("First items: " ^ (string_of_point_list first_items));
+              print_endline ("Rest: " ^ (String.concat " " (List.map string_of_point_list rest))); *)
+              first_items @ flat_transpose (rest)
+
+
+(* part 2 *)
+let alignments = "day10.dat"
+  |> read_lines
+  |> points_from_input
+  |> build_alignment_from spot
+  |> AlignedPoints.remove {x=0; y=0}
+
+let flattened = AlignedPoints.fold (fun k v a -> a@[v]) alignments []
+  |> flat_transpose
+
+let target_delta = List.nth flattened 199
+let target = {x = spot.x + target_delta.x; y = spot.y - target_delta.y}
+let () = Printf.printf "Part 2: %d\n" (target.x * 100 + target.y)
